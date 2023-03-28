213582dd4585974b0e162f8069147be9cd6b833f
MasterZorigh <73075421+MasterZorigh@users.noreply.github.com>
2023-03-28 13:05:20 +0200

    UPDATE : new changes 27-03

--- iut-app/src/app/core/core.module.ts
+++ iut-app/src/app/core/core.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    SharedModule,
+  ],
+
+  exports: [NavigationComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
